fd27539f0cfba07dc7f8427220e3c0bf3981596e
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-19 03:40:46 -0800

    fix errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,13 +5,13 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(0, list_length):
         try:
             temp_result = my_list_1[i] / my_list_2[i]
-        except (TypeError):
+        except TypeError:
             temp_result = 0
             print("wront type")
-        except (ZeroDivisionError):
+        except ZeroDivisionError:
             temp_result = 0
             print("division by 0")
-        except (IndexError):
+        except IndexError:
             temp_result = 0
             print("out of range")
         finally:
