841d72c30462963b3b658f096d1b43bc76ba77ca
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-20 09:52:10 -0800

    Fix errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,10 +44,11 @@ class Square:
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def my_print(self):
         """Prints the square
