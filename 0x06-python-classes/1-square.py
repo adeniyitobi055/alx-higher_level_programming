888ad43764593b28534fb7ed194bbc0225b40ae5
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-20 05:07:35 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class named square"""
+
+
+class Square:
+    """Defines a function called __init__"""
+    def __init__(self, size):
+        """Initializes __size of self with size"""
+        self.__size = size
+
