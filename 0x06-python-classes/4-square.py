a2059b981e64bdb98bb81bdfa2309520cde859a5
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-20 06:05:27 -0800

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """define __init__ function"""
+    def __init__(self, size=0):
+        """initialize __size of self with size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieve __size of self"""
+        return self.__size
+
+    @property.setter
+    def size(self, value):
+        """if statement"""
+        if type(value) != int:
+            """raise error"""
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            """raise error"""
+            raise ValueError("size must be >= 0")
+        else:
+            """initialize"""
+            self.__size = value
+    """define area function"""
+    def area(self):
+        return self.__size ** 2
