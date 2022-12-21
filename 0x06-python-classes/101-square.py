cc513037069c29ca3e77d7f574eeb2611f85da85
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2022-12-21 00:17:15 -0800

    Fix coding style error

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -64,6 +64,6 @@ class Square:
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size -1:
+            if i != self.__size - 1:
                 print("")
         return ("")
