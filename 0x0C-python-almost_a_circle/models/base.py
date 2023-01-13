39ba407a418f55d1f32933e0433c70263d602ea4
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-01-13 08:54:53 -0800

    update base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,7 @@ import os
 import csv
 import turtle
 
+
 class Base:
     """
     Base implementation
@@ -25,3 +26,120 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the json string representation of list_dictionaries.
+        Args:
+            list_dictionaries: is a list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file.
+        Args:
+            list_objs: is a list of instances who inherits from Base
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is None or list_objs = []:
+            lst = "[]"
+        else:
+            lst = cls.to_json_string([o.to_dictionary() for o in list_objs])
+        with open(filename, "w") as a_file:
+            a_file.write(lst)
+
+        return lst
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string.
+        Args:
+            json_string: is a string representating a list of dictionaries
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @staticmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set.
+        Args:
+            dictionary: can be thought of as a double pointer to a dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        returns a list of instances.
+        """
+        filename = cls.__name__ + ".json"
+        lst = []
+        if os.path.exists(filename):
+            with open(filename, "r") as a_file:
+                s = a_file.read()
+                list_dicts = cls.from_json_string(s)
+                for d in list_dicts:
+                    lst.append(cls.create(**d))
+        return lst
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Serializes list_objs in CSV formats and saves it to a file
+        Args:
+            list_objs: list of instances
+        """
+        if (type(list_objs) != list and list_objs is not None
+                or not all(isinstance(x, cls) for x in list_objs)):
+            raise TypeError("list_objs must be a list of instances")
+
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w") as a_file:
+            if list_objs is not None:
+                list_objs = [x.to_dictionary() for x in list_objs]
+                if cls.__name__ == "Rectangle":
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == "Square":
+                    fields = ['id', 'size', 'x', 'y']
+                writer = csv.DictWriter(f, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Deserializes CSV format from a file.
+        Returns: list of instances
+        """
+        filename = cls.__name__ + ".csv"
+        lst = []
+        if os.path.exists(filename):
+            with open(filename, "r") as a_file:
+                reader = csv.reader(f, delimiter=',')
+                if cls.__name__ == "Rectangle":
+                    fields = ['id', 'width', 'height', 'x', 'y']
+                elif cls.__name__ == "Square":
+                    fields = ['id', 'size', 'x', 'y']
+                for x, row in enumerate(reader):
+                    if x > 0:
+                        i = cls(1, 1)
+                        for j, e in enumerate(row):
+                            if e:
+                                setattr(i, field[j], int(e))
+                        lst.append(i)
+        return lst
