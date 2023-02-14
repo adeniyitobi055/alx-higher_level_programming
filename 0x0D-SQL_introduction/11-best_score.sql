1223066fc3ec821f601e9b3933780e97e801b1ce
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-14 05:12:35 -0800

    Fix error

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in a table second_table of a database
-SELECT * FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
