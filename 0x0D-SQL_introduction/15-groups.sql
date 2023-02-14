99166c0b62f51804b98e97eba476e836c09a1e84
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-14 05:38:13 -0800

    groups

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- lists the number of records with the same score in the table second_table of a database
+SELECT `score` COUNT(`score`) `number` FROM second_table GROUP BY `score` ORDER BY `number` DESC;
