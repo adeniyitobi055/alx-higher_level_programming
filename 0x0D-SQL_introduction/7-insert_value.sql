47d8d81fea925e22703a32d672a0a6639157edf2
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-14 04:25:48 -0800

    Fix error

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in a table in a database
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, `Best School`);
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
