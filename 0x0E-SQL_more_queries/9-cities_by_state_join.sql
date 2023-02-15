be13ff0be7171846b7988b8894530fa09ca7cbc9
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 08:07:30 -0800

    fix error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in a database
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON state.id = cities.states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state.id ORDER BY cities.id;
