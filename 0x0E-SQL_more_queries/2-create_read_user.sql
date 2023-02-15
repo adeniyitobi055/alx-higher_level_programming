e4660532922f26f7d0cd87ee51ab1c35f9ca5316
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 06:19:12 -0800

    fix error

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create a user user_0d_2
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- grant privileges
-GRANT SELECT PRIVILEGE ON hbtn_0d_2 . * TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
