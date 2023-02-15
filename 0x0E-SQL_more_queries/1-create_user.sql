53f792c5542a073952f71c71bb2a6c3ce9fbd8b4
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 05:45:56 -0800

    fix error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,6 @@
 -- user_0d_1 would have all privileges on your MySQL server
 -- The user_0d_1 password would be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, it would not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- grant priviledges
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
