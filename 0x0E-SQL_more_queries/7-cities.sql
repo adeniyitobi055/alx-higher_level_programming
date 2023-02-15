ee3829e3b03c0ab46bbe277d55fa43df536785eb
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 07:17:48 -0800

    fix error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,10 +8,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- if table already exist it doesn't fail
 CREATE TABLE IF NOT EXISTS cities
 (
-	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	states_id INT NOT NULL,
 	name VARCHAR(256),
 	PRIMARY KEY(id),
 	FOREIGN KEY(states_id)
-	REFERENCES hbtn_0d_usa.states(id)
+		REFERENCES hbtn_0d_usa.states(id)
 );
