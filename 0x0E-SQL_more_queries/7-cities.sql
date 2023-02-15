8278f3a2af1e0fce155599ad43366c0e02116c61
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 07:11:01 -0800

    cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,16 @@
+-- creates the database hbtn_0d_usa and the table cities
+-- creates a database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- creates a table cities
+-- id INT unique, auto-generated, not null, primary key
+-- states_id INT, not null, foreign key that references id 'states' table
+-- name varchar not null
+-- if table already exist it doesn't fail
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT NOT NULL UNIQUE AUTO_INCREMENT,
+	states_id INT NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(states_id)
+	REFERENCES hbtn_0d_usa.states(id)
+);
