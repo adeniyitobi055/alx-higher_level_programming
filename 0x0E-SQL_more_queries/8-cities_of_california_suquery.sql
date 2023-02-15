378e161c2c66220f3c279031bb9e17ecc1c30a0e
Oluwatobi Adeniyi <adedejiadeniyi055@gmail.com>
2023-02-15 16:46:06 +0100

    Delete 8-cities_of_california_suquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_suquery.sql
+++
@@ -1,5 +0,0 @@
--- lists all the cities of california that can be found in the database hbtn_0d_usa
--- lists al cities
-SELECT id, name FROM cities WHERE state_id = (
-	SELECT id FROM states WHERE name  = 'California'
-) ORDER BY id ASC;
