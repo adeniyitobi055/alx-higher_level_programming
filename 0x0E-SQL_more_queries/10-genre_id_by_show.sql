c266582e1abb8dc02fe14c578cfcb86cd7dbae87
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-15 09:34:10 -0800

    fix error

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_show INNER JOIN tv_show_genre ON tv_shows.id = tv_shows_genre.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show INNER JOIN tv_show_genre ON tv_shows.id = tv_shows_genre.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
