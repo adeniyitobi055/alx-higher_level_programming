1bc6c91d8e7309ae77fea2d51c7a3f0f6699b202
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-16 01:57:27 -0700

    fix codestyle error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port='3306', user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states where name
