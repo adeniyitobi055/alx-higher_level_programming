afa5b6572ac8822133a3d9191fcdf4fa92d00e2b
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-16 02:01:00 -0700

    Create database & table if not exists

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
-);
+	);
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
