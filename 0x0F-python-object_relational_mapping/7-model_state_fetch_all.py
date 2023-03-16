6e4598b2aa25b8119412be65e6fe9ad8acd278ef
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-16 06:34:42 -0700

    Print states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+7-model_state_fetch_all module
+"""
+from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3], pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print('{}: {}'.format(state.id, state.name))
