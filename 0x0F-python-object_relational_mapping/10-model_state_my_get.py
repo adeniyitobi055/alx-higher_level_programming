0cd3ecbefdecb5a9d12d4566080cf92a2e9b5841
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-16 08:48:04 -0700

    get id of a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+10-model_state_my_get module
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3], pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if state:
+        print('{}'.format(state.id))
+    else:
+        print('Not found')
+
+    session.close()
