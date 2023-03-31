4dd2b63e05dacab89d195316d15b9d944b3673da
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-31 06:32:40 -0700

    update 4-hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     """ Fetches http://alx-intranet.hbtn.io/status """
-    r = requests.get("http://alx-intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
