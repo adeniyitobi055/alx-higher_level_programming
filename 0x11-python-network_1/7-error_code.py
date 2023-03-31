e80597c0dd43d4bc10329035955f409773a8742c
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-31 07:31:26 -0700

    update 7-error_code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -15,4 +15,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    print(r.status_code)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
