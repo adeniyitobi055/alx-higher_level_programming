e39bf17e4203272565cfbe45e7ee5ea3a60d21a4
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-31 04:40:50 -0700

    fix 2-post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     and displays the body of the response (decoded in utf-8)
     """
     url = sys.argv[1]
-    value = {"email" : sys.argv[2]}
+    value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
