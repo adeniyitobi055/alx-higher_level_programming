c00d7dbabab7a0930f7c39081fdd4594b81c30bf
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-03-31 08:51:17 -0700

    add 10-my_github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Takes your Github credentials username and password and uses
     the GitHub API to display your id
     """
-    
+
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
