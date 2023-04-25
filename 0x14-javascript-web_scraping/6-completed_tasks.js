91c5bcbdeb29f67bf4b8775150f372901c0be0a5
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-04-25 04:25:09 -0700

    add 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const tasksCompleted = {};
+  const data = JSON.parse(body);
+  data.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
